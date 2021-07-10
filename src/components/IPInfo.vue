46c607e856a335b53706c71aa1ad06db3da1ab8a
Mahabubul Hasan <shourav6688@gmail.com>
2021-07-10 14:55:18 +0600

    feat: map added

---
+++ src/components/IPInfo.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="absolute -bottom-64 lg:-bottom-20 flex flex-col lg:flex-row bg-white gap-y-4 gap-x-12 px-8 pt-8 pb-12 rounded-md">
+    <div class="flex flex-col">
+      <h3 class="text-xs mb-2 uppercase">IP Address</h3>
+      <span class="text-2xl">8.8.8.8</span>
+    </div>
+    <div class="flex flex-col">
+      <h3 class="text-xs mb-2 uppercase">Location</h3>
+      <span class="text-2xl">Dhaka</span>
+    </div>
+    <div class="flex flex-col">
+      <h3 class="text-xs mb-2 uppercase">Timezone</h3>
+      <span class="text-2xl">UTC +6</span>
+    </div>
+    <div class="flex flex-col">
+      <h3 class="text-xs mb-2 uppercase">ISP</h3>
+      <span class="text-2xl">Next Trun</span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,82 @@
 <template>
   <div class="flex flex-col h-screen max-h-screen">
     <!-- search  -->
-    <div class="flex justify-center relative px-4 pt-8 pb-32"></div>
+    <div
+      class="
+        z-20
+        flex
+        justify-center
+        relative
+        bg-hero-pattern bg-cover
+        px-4
+        pt-8
+        pb-32
+      "
+    >
+      <div class="w-full max-w-screen-sm">
+        <h1 class="text-white text-center text-3xl pb-4">IP Address Tracker</h1>
+        <div class="flex">
+          <input
+            class="
+              flex-1
+              py-3
+              px-2
+              rounded-tl-md rounded-bl-md
+              focus:outline-none
+            "
+            placeholder="Search for any IP Address or leave it empty to get your IP Info"
+            type="text"
+          />
+          <i
+            class="
+              cursor-pointer
+              bg-black
+              text-white
+              px-4
+              rounded-tr-md rounded-br-md
+              flex
+              items-center
+              fas
+              fa-chevron-right
+            "
+          ></i>
+        </div>
+      </div>
+      <IPInfo />
+    </div>
 
+    <!-- Map -->
+    <div id="mapid" class="h-full z-10"></div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-
+import leaflet from "leaflet";
+import IPInfo from "../components/IPInfo";
+import { onMounted } from "vue";
 export default {
-  name: 'Home',
+  name: "Home",
   components: {
-    
-  }
-}
+    IPInfo,
+  },
+  setup() {
+    let mymap;
+    onMounted(() => {
+      mymap = L.map("mapid").setView([51.505, -0.09], 13);
+      L.tileLayer(
+        "https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWhhc2FuMDkiLCJhIjoiY2txeGhsbW9iMHZubjJucWhqaG5hd2Q2NCJ9.FVvSMcgOkFEfTRXxIuJS3g",
+        {
+          attribution:
+            'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+          maxZoom: 18,
+          id: "mapbox/streets-v11",
+          tileSize: 512,
+          zoomOffset: -1,
+          accessToken: "pk.eyJ1IjoibWhhc2FuMDkiLCJhIjoiY2txeGhsbW9iMHZubjJucWhqaG5hd2Q2NCJ9.FVvSMcgOkFEfTRXxIuJS3g",
+        }
+      ).addTo(mymap);
+    });
+  },
+};
 </script>
